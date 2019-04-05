84cbc9dddd11b2c339feac8ee7e1d5fdad705149
Eric Phillips <dephil29@hotmail.com>
2019-04-05 17:00:32 -0400

    trashed App.js, build and serviceworker, reorganized index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
+// dependencies
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+// components
+import Header from './components/Header';
+import Main from './components/Main';
+//style
+import './index.css';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-,document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  <BrowserRouter >
+    <div className="thewholething">
+      <Header />
+      <Main />
+    </div>
+  </BrowserRouter>,document.getElementById('root')
+);
