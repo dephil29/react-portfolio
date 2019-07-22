869bedbd89e68900730df98078684c022b5ea9a7
Eric Phillips <dephil29@hotmail.com>
2019-07-22 14:51:27 -0400

    added flickr, removed ned

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,28 +1,37 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 
-import scrollToTop from '../scrollToTop/scrollToTop';
+import scrollToTop from "../scrollToTop/scrollToTop";
 
 const Contact = () => {
-
   useEffect(() => {
-    document.title = 'contact - ericphillips.xyz'
+    document.title = "contact - ericphillips.xyz";
   });
 
   scrollToTop();
 
   return (
-    <div 
-      className="content contact"
-    >
-      <h2>eric@ericphillips.xyz</h2>
+    <div className="content contact">
+      <h2>dephil29@hotmail.com</h2>
       <h2>
-        <a href="https://www.github.com/dephil29" rel="noopener noreferrer" target="_blank">github</a>
+        <a
+          href="https://www.github.com/dephil29"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          github
+        </a>
       </h2>
       <h2>
-        <a href="https://www.linkedin.com/in/eric-phillips-19a287bb/" rel="noopener noreferrer" target="_blank">linkedin</a>
+        <a
+          href="https://www.linkedin.com/in/eric-phillips-19a287bb/"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          linkedin
+        </a>
       </h2>
     </div>
   );
-}
+};
 
 export default Contact;
--- src/components/Projects/info.js
+++ src/components/Projects/info.js
@@ -1,74 +1,77 @@
-import Meattown from '../../images/meattown.jpg';
-import Little from '../../images/little.jpg';
-import Ned from '../../images/ned.jpg';
-import Atlar from '../../images/atlar.png';
-import Smartgurlz from '../../images/smartgurlz.jpg';
-import Hacker from '../../images/hacker.jpg';
-import Burger from '../../images/burger.png';
-import Forum from '../../images/forum.png';
+import Meattown from "../../images/meattown.jpg";
+import Little from "../../images/little.jpg";
+import Atlar from "../../images/atlar.png";
+import Smartgurlz from "../../images/smartgurlz.jpg";
+import Hacker from "../../images/hacker.jpg";
+import Burger from "../../images/burger.png";
+import Forum from "../../images/forum.png";
+import Flickr from "../../images/flickr.png";
 
 const data = [
   {
-    "projectType": "professional",
-    "link": "https://smartgurlz.com/",
-    "imgSrc": Smartgurlz,
-    "title": "Smartgurlz",
-    "type": "Shopify",
-    "desc": "Several custom features on the home page and product pages are completely hand coded according to the clients wishes."
+    projectType: "professional",
+    link: "https://smartgurlz.com/",
+    imgSrc: Smartgurlz,
+    title: "Smartgurlz",
+    type: "Shopify",
+    desc:
+      "Several custom features on the home page and product pages are completely hand coded according to the clients wishes."
   },
   {
-    "projectType": "professional",
-    "link": "https://mylittleandi.com/",
-    "imgSrc": Little,
-    "title": "My Little and I",
-    "type": "Shopify",
-    "desc": "Original Instagram display section, and collection pages designed specifically for multiple product orders."
+    projectType: "professional",
+    link: "https://mylittleandi.com/",
+    imgSrc: Little,
+    title: "My Little and I",
+    type: "Shopify",
+    desc:
+      "Original Instagram display section, and collection pages designed specifically for multiple product orders."
   },
   {
-    "projectType": "professional",
-    "link": "https://helloned.com/",
-    "imgSrc": Ned,
-    "title": "Ned",
-    "type": "Shopify",
-    "desc": "Custom home page sections for product information display. Several custom product page templates for unique pages for each product. Heavily redesigned header and mega menu."
+    projectType: "professional",
+    link: "https://meattown.com/",
+    imgSrc: Meattown,
+    title: "MeatTown",
+    type: "Shopify",
+    desc:
+      "Custom sections for the home page and customization of the product pages in accordance with clients wishes."
   },
   {
-    "projectType": "professional",
-    "link": "https://meattown.com/",
-    "imgSrc": Meattown,
-    "title": "MeatTown",
-    "type": "Shopify",
-    "desc": "Custom sections for the home page and customization of the product pages in accordance with clients wishes."
+    projectType: "professional",
+    link: "https://theatlar.com/",
+    imgSrc: Atlar,
+    title: "The Atlar",
+    type: "Shopify",
+    desc:
+      "This was more of a triumph of creative inventory management to make sure that the correct products showed up in their proper collections than anything to do with web design."
   },
   {
-    "projectType": "professional",
-    "link": "https://theatlar.com/",
-    "imgSrc": Atlar,
-    "title": "The Atlar",
-    "type": "Shopify",
-    "desc": "This was more of a triumph of creative inventory management to make sure that the correct products showed up in their proper collections than anything to do with web design."
+    projectType: "personal",
+    link: "https://ericphillips.xyz/gdghghsdf",
+    imgSrc: Flickr,
+    title: "Flickr Photo Search",
+    desc: "Photo search app using flickr api."
   },
   {
-    "projectType": "personal",
-    "link": "https://ericphillips.xyz/hacker-news",
-    "imgSrc": Hacker,
-    "title": "Hacker News",
-    "desc": "React, API and search functionality practice."
+    projectType: "personal",
+    link: "https://ericphillips.xyz/hacker-news",
+    imgSrc: Hacker,
+    title: "Hacker News",
+    desc: "React, API and search functionality practice."
   },
   {
-    "projectType": "personal",
-    "link": "https://ericphillips.xyz/burger-builder",
-    "imgSrc": Burger,
-    "title": "Burger Builder",
-    "desc": "React/Redux/Firebase burger ordering machine."
+    projectType: "personal",
+    link: "https://ericphillips.xyz/burger-builder",
+    imgSrc: Burger,
+    title: "Burger Builder",
+    desc: "React/Redux/Firebase burger ordering machine."
   },
   {
-    "projectType": "personal",
-    "link": "https://jabandaja.com/generic_forum",
-    "imgSrc": Forum,
-    "title": "Generic Forum",
-    "desc": "Forum site built with PHP/CodeIgniter."
+    projectType: "personal",
+    link: "https://jabandaja.com/generic_forum",
+    imgSrc: Forum,
+    title: "Generic Forum",
+    desc: "Forum site built with PHP/CodeIgniter."
   }
 ];
 
-export {data};
+export { data };
